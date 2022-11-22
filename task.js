eee2eff47518a92465f24bf2959396b9f065dd65
varshapatil1234 <118799668+varshapatil1234@users.noreply.github.com>
2022-11-22 02:18:26 -0800

    Add files via upload

---
+++ task.js
@@ -0,0 +1,15 @@
+function changeStyleClockwise(){
+    document.getElementById('red').style.backgroundColor='orange';
+    document.getElementById('yellow').style.backgroundColor='red';
+    document.getElementById('green').style.backgroundColor='yellow';
+    document.getElementById('orange').style.backgroundColor='green';
+}  
+
+function changeStyleAntiClockwise(){
+
+   document.getElementById('orange').style.backgroundColor='red';
+   document.getElementById('green').style.backgroundColor='orange';
+   document.getElementById('yellow').style.backgroundColor='green';
+   document.getElementById('red').style.backgroundColor='yellow';
+  
+}
